f618f6c9121fe26e0b219d2558e36aa5ff589cef
merve kartal <mervekartal@merves-MacBook-Air.local>
2023-02-22 18:18:57 +0300

    static files and path module imported to render index page

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const path = require('path')
+
+
 
 const app = express()
 
 
 
 
+app.use(express.static('public'))
 
 
 
-
-
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/index.html'))
+})
 
 
 const port = 3000
