24a51a20b7c50a4b4c56dc1a4b0ccaf6c161c451
merve kartal <mervekartal@merves-MacBook-Air.local>
2023-03-01 18:15:14 +0300

    pagination operations

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -12,11 +12,25 @@ exports.getAboutPage = (req,res) => {
     })
 }
 exports.getCategoriesPage = async (req,res) => {
-    const categories = await Category.find().sort('-createdAt')
-    res.status(200).render('categories',{
-        page_name: "categories",
-        categories
+    //const categories = await Category.find().sort('-createdAt')
+    const page = req.query.page || 1 //request'ten gelen sayfa numarası, yoksa sayfa
+    const categoriesPerPage = 2  //1 sayfada gösterilecek foto sayısı
+    const totalCategory = await Category.find().countDocuments() //toplam foto sayısı
+
+    //sayfa geçişi
+    const categories = await Category.find({}).sort('-dateCreated')
+    .skip((page-1) * categoriesPerPage)
+    .limit(4)
+    res.status(200).render('categories', {
+      page_name: "categories",
+      categories, 
+      current: page,
+      pages: Math.ceil(totalCategory / categoriesPerPage)
     })
+    // res.status(200).render('categories',{
+    //     page_name: "categories",
+    //     categories
+    // })
 }
 exports.getFurnituresPage = async (req,res) => {
     // const furnitures = await Furniture.find().sort('-createdAt')
